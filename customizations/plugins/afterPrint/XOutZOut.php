7a0b5f49f92d516e52127ba46015e359eb2f289a
ayenDev99 <anchetakaren34@gmail.com>
2025-05-29 13:52:50 +0800

    XZOut : Revise computation for TOTAL IN DRAWER
    Formula:
    OPEN AMOUNT + CASH SALES _ DISBURSEMENT PAID IN - DISBURSEMENT PAID OUT - CASH DROP (LESS)

--- customizations/plugins/afterPrint/XOutZOut.php
+++ customizations/plugins/afterPrint/XOutZOut.php
@@ -415,7 +415,12 @@
                         $data[$key] = (isset($results['CASH_DROP_LESS'])) ? number_format($results['CASH_DROP_LESS'], 2) : '0.00' ;  
                         break;
                     case 'value#total_in_drawer':
-                        $data[$key] = (isset($results['TOTAL_IN_DRAWER'])) ? number_format($results['TOTAL_IN_DRAWER'], 2) : '0.00' ;  
+                        $data[$key] =
+                            number_format(((isset($results['OPEN_AMOUNT'])) ? $results['OPEN_AMOUNT'] : 0) +
+                            ((isset($results['CASH_PAYMENTS'])) ? $results['CASH_PAYMENTS'] : 0) +
+                            ((isset($results['DISB_PAID_IN'])) ? $results['DISB_PAID_IN'] : 0) -
+                            ((isset($results['DISB_PAID_OUT'])) ? $results['DISB_PAID_OUT'] : 0) -
+                            ((isset($results['CASH_DROP_LESS'])) ? $results['CASH_DROP_LESS'] : 0), 2);  
                         break;
                     case 'value#cash_count_declaration':
                         $data[$key] = (isset($results['CASH_COUNT_DECLARATION'])) ? number_format($results['CASH_COUNT_DECLARATION'], 2) : '0.00' ;  
